6dbaed21c49368540aa7e1188a48d6c2e94e2937
web-collab <tanmoy587@gmail.com>
2025-03-19 11:43:51 +0530

    mongodb connection

---
+++ server/config/db.js
@@ -0,0 +1,24 @@
+//Database Connection 
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+const mongoURI = process.env.MONGO_URI;
+
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(mongoURI
+        //   ,{
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true
+        // }
+      );
+        console.log('MongoDB connected successfully'); // Log success message if connected
+    } catch (error) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require('mongoose'); // Import Mongoose for MongoDB
 const studentRouter = require('./router/studentRouter.js');
-
+const connectDB = require("./config/db");
 
 //dotenv configuartion
 dotenv.config();
 const app = express();
 
+
+// MongoDB connection
+connectDB();
 
 app.get("/",(req,res)=>{
     res.send("<h1>Welcome To Student Management Server</h1>")
