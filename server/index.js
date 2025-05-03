7cb3ce9f839306483da5d9d28f93dc0757761834
tanmayMt <tanmoy587@gmail.com>
2025-05-03 17:22:18 +0530

    Admin Logig & Admin Control port change for hosting

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ connectDB();
 
 // ✅ CORS Configuration
 const corsOptions = {
-    // origin: "http://localhost:3000", // Allow frontend requests
-    origin: "https://student-management-system-using-mern.vercel.app",    // https://student-management-system-using-mern.vercel.app
+    origin: "http://localhost:3000", // Allow frontend requests
+    //origin: "https://student-management-system-using-mern.vercel.app",    // https://student-management-system-using-mern.vercel.app
     methods: "GET,POST,PUT,DELETE",
     credentials: true // Allow cookies and authentication headers
 };
